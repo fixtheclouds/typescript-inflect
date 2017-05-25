aa06e620c163be784601fc397f046bf251d6a9dd
Egor Romanov <unsaidxpl@gmail.com>
2017-05-25 09:15:40 +0300

    Add some functions

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 export default class Inflect {
-  demodulize(string : string) : string {
+  demodulize(string: string): string {
     const delimiter = '::';
     const index = string.lastIndexOf(delimiter);
     return index >= 0 ? string.substring(index + delimiter.length) : string;
+  }
+
+  camelize(string: string, lowercaseFirstLetter?: boolean): string {
+    let result = lowercaseFirstLetter ?
+      string[0].toLowerCase() + string.substr(1) : string[0].toUpperCase() + string.substr(1);
+    return result.replace(/[_.-](\w|$)/g, (_: string, l: string) =>
+      l.toUpperCase()
+    );
+  }
+
+  dasherize(string: string): string {
+    return string.replace(/_/g, '-');
   }
 }
--- test/InflectTest.ts
+++ test/InflectTest.ts
@@ -12,13 +12,25 @@ describe('Inflect', () => {
 
   describe('#demodulize', () => {
     it('return last part of module string', () => {
-      let result: string = subject.demodulize('Test::String::Module');
-      assert.equal('Module', result);
+      assert.equal('Module', subject.demodulize('Test::String::Module'));
     });
     it('returns the whole string if it`s not module string', () => {
-      let result: string = subject.demodulize('Something');
-      assert.equal('Something', result);
+      assert.equal('Something', subject.demodulize('Something'));
     });
   });
 
+  describe('#camelize', () => {
+    it('camelizes string', () => {
+      assert.equal('TestString', subject.camelize('test_string'));
+    });
+    it('camelizes string with lower case first letter is told so', () => {
+      assert.equal('testString', subject.camelize('Test_string', true));
+    });
+  });
+
+  describe('#dasherize', () => {
+    it('dasherizes string', () => {
+      assert.equal('un-der-score', subject.dasherize('un_der_score'));
+    });
+  });
 });
